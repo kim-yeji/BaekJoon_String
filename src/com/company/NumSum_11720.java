67c2d7f324de351dbae76226b62179a6bcc1b572
yeji <aa960923@naver.com>
2020-06-24 20:34:58 +0900

    20200624 wed

---
+++ src/com/company/NumSum_11720.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+//  *+..+ 11720번 숫자의 합 +..+*
+/*
+public class NumSum_11720 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        String s = scan.next();
+
+
+        for (int i = 0 ; i < num ; i++){
+            sum += s.charAt(i)-'0';
+            // sum += Integer.parseInt(s.substring(i,i+1));
+        }
+
+        System.out.print(sum);
+    }
+}
+
+//문자를 숫자로 변환하는 방법
+//1. 스트링.charAt(변환하고 싶은 위치)
+//2. Integer.parseInt("스트링") 인데, 하나하나 쪼개서 더하고 싶을 때는 스트링.substring(i,i+1)까지 사용하면 됨
+
+
+*/
