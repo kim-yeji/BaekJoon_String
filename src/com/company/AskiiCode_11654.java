2dc873a9aec4ead182a4003e985a3a807318d184
yeji <aa960923@naver.com>
2020-06-25 20:31:54 +0900

    2020062 thu

--- src/com/company/AskiiCode_11654.java
+++ src/com/company/AskiiCode_11654.java
@@ -1,4 +1,21 @@
 package com.company;
+import java.util.Scanner;
 
+//  *+..+ 11654번 아스키 코드 +..+*
+
+/*
 public class AskiiCode_11654 {
+    public int solution(String s){
+        int answer = s.charAt(0);  //String -> Int
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+
+        Main main = new Main();
+        int num = main.solution(s);
+        System.out.print(num);
+    }
 }
+*/
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        String s = scan.next();
+
+
+        for (int i = 0 ; i < num ; i++){
+            sum += s.charAt(i)-'0';
+            // sum += Integer.parseInt(s.substring(i,i+1));
+        }
+
+        System.out.print(sum);
     }
 }
